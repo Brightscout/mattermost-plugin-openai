1aaecc09ce2ac1e017c30cfa20fd5a0bcb168831
Abhishek Verma <abhishek.verma@brightscout.com>
2023-04-19 18:03:51 +0530

    [MOI-43]: Added blocks

--- webapp/src/utils/index.ts
+++ webapp/src/utils/index.ts
@@ -272,11 +272,14 @@ export const getLastValue = (completeString: string, splitBy = ' ') => {
 export const getImageExpiryTime = (createdAtTimestamp: string) => {
     // we are converting "createdAt" into milliseconds and adding the duration for expiry with buffer.
     const date = new Date((parseInt(createdAtTimestamp, 10) * 1000) + DURATION_FOR_IMAGE_EXPIRY_IN_MILLISECONDS_WITH_BUFFER);
+
     let hours = date.getHours();
     let minutes: number | string = date.getMinutes();
     const ampm = hours >= 12 ? 'pm' : 'am';
+
     hours %= 12;
     hours = hours || 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0' + minutes : minutes;
+
     return hours + ':' + minutes + ' ' + ampm;
 };
